1d21344743b575f7f757c8451061cced9dfe5458
Keith Noguchi <keith@noguchi.us>
2022-10-02 19:47:19 -0700

    index.js: Show the balance of the wallet

--- index.js
+++ index.js
@@ -11,5 +11,18 @@ const wallet = new Keypair()
 const publicKey = new PublicKey(wallet._keypair.publicKey)
 const privateKey = wallet._keypair.secretKey
 
-console.log(publicKey)
-console.log(privateKey)
+const getWalletBalance = async() => {
+    try {
+        const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+        const walletBalance = await connection.getBalance(publicKey)
+        console.log(`Wallet balance is ${walletBalance}`)
+    } catch(e) {
+        console.error(e)
+    }
+}
+
+const main = async() => {
+    await getWalletBalance()
+}
+
+main()
